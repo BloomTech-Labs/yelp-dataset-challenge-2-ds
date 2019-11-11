af8fd90263bc4ea5d659b83bdb6587f71d5d7340
Vincent Brandon <vincenta..brandon@gmail.com>
2019-11-11 17:55:43 +0000

    changed logger handler to gunicorn logger

--- db_api/app.py
+++ db_api/app.py
@@ -95,7 +95,8 @@ def create_app(test_config=None):
     # Change logging.INFO to logging.DEBUG to get full logs.  Will be a crapload of information.
     # May significantly impair performance if writing logfile to disk (or network drive).
     # To enable different services, see README.md
-    logging.basicConfig() #  Default logging behavior
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
     # logging.basicConfig(filename=app.config['LOGFILE'], level=logging.INFO)  # File logging
     logging.getLogger('flask_cors').level = logging.INFO
     app_logger = logging.getLogger(__name__)
