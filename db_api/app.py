36555f4ce644b1b5244c833a87d4a3963b384819
Vincent Brandon <vincenta..brandon@gmail.com>
2019-11-11 21:08:01 +0000

    updated procfile for info debug.

--- db_api/app.py
+++ db_api/app.py
@@ -36,7 +36,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     # If environment vairables not set, will default to development expected paths and names
     app.config.from_mapping(
-        DEBUG=config('DEBUG', default=True),  # Make sure to change debug to False in production env
+        DEBUG=config('DEBUG', default=False),  # Make sure to change debug to False in production env
         SECRET_KEY=config('SECRET_KEY', default='dev'),  # CHANGE THIS!!!!
         DATABASE_URI=config('DATABASE_URI', 'sqlite:///' + os.path.join(os.getcwd(), local_db_name)),  # For in-memory db: default='sqlite:///:memory:'),
         LOGFILE=config('LOGFILE', os.path.join(app.instance_path, 'logs/debug.log')),
