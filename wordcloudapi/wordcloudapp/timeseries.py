b876109d2df9be70b6602e1b9513c1dd285c270f
Nayomi Chibana <nchibana@gmail.com>
2019-11-13 10:34:25 -0500

    refactored code

--- wordcloudapi/wordcloudapp/timeseries.py
+++ wordcloudapi/wordcloudapp/timeseries.py
@@ -4,79 +4,42 @@ import ast
 from collections import Counter
 from .models import DB, reviews
 
-def count(docs):
-
-        word_counts = Counter()
-        total_docs = len(docs)
-
-        for doc in docs:
-            word_counts.update(doc)
-
-        temp = zip(word_counts.keys(), word_counts.values())
-        
-        wc = pd.DataFrame(temp, columns = ['word', 'count'])
-
-        wc['rank'] = wc['count'].rank(method='first', ascending=False)
-        total = wc['count'].sum()
-
-        wc['pct_total'] = wc['count'].apply(lambda x: x / total)
-        
-        wc = wc.sort_values(by='rank')
-
-        return wc
+def wc_count(docs):
+    """Count the occurance of each word and rank
+    """
+    total=len(docs)
+    wc = pd.DataFrame({'word':docs, 'count':np.ones(len(docs))})
+    wc = wc.groupby('word').sum()
+    wc['pct_total'] = wc['count']/total
+    wc['rank'] = wc['count'].rank(method='first', ascending=False)
+    return wc.sort_values(by='rank').nlargest(30, 'count')
 
 
 def timeseries(bus_id):
 
-    result = reviews.query.filter_by(business_id=bus_id)
+    result = reviews.query.with_entities(reviews.tokens, reviews.date, \
+             reviews.star_review).filter_by(business_id=bus_id)
     df = pd.read_sql(sql = result.statement, con = DB.engine)
+    df['tokens'] = df['tokens'].str.strip('\[').str.strip('\]').\
+        str.split(', ')
     filtered = df.sort_values('date')
-    no_reviews = len(filtered)
-    filtered['tokens'] = filtered['tokens'].apply(ast.literal_eval)
-    x = no_reviews//10
-    output = {}
-    dictionaries = []
-    def listoflists(lst):
-        return [[el] for el in lst]
-    if no_reviews >= 20:
-        agg_tokens = np.add.reduceat(filtered['tokens'].values, \
-                                     np.arange(0, len(filtered['tokens']), x))
-        nth = filtered.iloc[::x, :]
-        date = nth['date'].values
-        avg_stars = np.add.reduceat(filtered['star_review'].values, \
-                    np.arange(0, len(filtered['star_review']), x))/ \
-                    np.bincount(np.resize(np.repeat(np.arange(0,(len(filtered['star_review'])//x)+1), x), \
-                    len(filtered['star_review'])))
-        new_df = pd.DataFrame({'tokens':agg_tokens, 'date':date, 'avg_stars': avg_stars})
-    else:
-        agg_tokens = filtered['tokens'].values
-        date = filtered['date'].values
-        avg_stars = filtered['star_review'].values
-        new_df = pd.DataFrame({'tokens':agg_tokens, 'date':date, 'avg_stars': avg_stars})
-    for i, ddate in enumerate(new_df['date']):
-        if len(set(new_df['tokens'].values[i])) < 30:
-            for j in range(len(set(new_df['tokens'].values[i]))):
-                extract = listoflists(new_df['tokens'].values[i])
-                wc = count(extract)
-                wc_final = wc[wc['rank'] <= 30]
-                row_dict = dict({"word": str(wc_final['word'].values[j]), \
-                                 "count": str(wc_final['count'].values[j]), \
-                                 "rank": str(wc_final['rank'].values[j]), \
-                                 "avg_stars": str(new_df['avg_stars'].values[i]), \
-                                 "pct_total": str(wc_final['pct_total'].values[j])})
-                dictionaries.append(row_dict)
-        else:
-            for j in range(30):
-                extract = listoflists(new_df['tokens'].values[i])
-                wc = count(extract)
-                wc_final = wc[wc['rank'] <= 30]
-                row_dict = dict({"word": str(wc_final['word'].values[j]), \
-                                 "count": str(wc_final['count'].values[j]), \
-                                 "rank": str(wc_final['rank'].values[j]), \
-                                 "avg_stars": str(new_df['avg_stars'].values[i]), \
-                                 "pct_total": str(wc_final['pct_total'].values[j])})
-                dictionaries.append(row_dict)
-        review_date = dict({str(ddate): dictionaries})
-        output.update(review_date)
-        dictionaries = []
+    filtered['bins'] = pd.qcut(filtered.index, q=10, precision=0)
+    new_df = filtered.groupby('bins').agg({'tokens': 'sum', \
+            'star_review': 'mean', 'date': lambda x: x.iloc[-1]})
+
+    counts = []
+    for i in range(len(new_df)):
+        wc_df = wc_count(new_df['tokens'].values[i])
+        wc_df['date'] = new_df['date'].values[i]
+        wc_df['star_review'] = new_df['star_review'].values[i]
+        counts.append(wc_df)
+
+    df_final = pd.concat(counts)
+    df_final['date'] = df_final['date'].astype(str)
+    df_final = df_final.reset_index()
+    output = (df_final.groupby(['date'], as_index=True)
+             .apply(lambda x: x[['word','count','pct_total','rank',\
+                 'star_review']].to_dict('r'))
+              .to_json())
+
     return output
