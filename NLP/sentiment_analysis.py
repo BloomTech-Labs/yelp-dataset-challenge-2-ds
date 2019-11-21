7bfbb1e3c94e5558e0bfb566840fcabfdca5b9ee
Scott-Huston <scottphuston@gmail.com>
2019-11-21 13:24:43 -0800

    fleshing out

--- NLP/sentiment_analysis.py
+++ NLP/sentiment_analysis.py
@@ -13,13 +13,13 @@ import logging
 import os
 
 # downloading spacy dependencies
-os.system('pip install spacy')
-import spacy
-os.system('python -m spacy download en_core_web_lg')
-nlp = spacy.load('en_core_web_lg') 
+os.system('pip install textblob')
+from textblob import TextBlob
 
-from jobs import get_jobs, pop_current_job, read_job,\
-     download_data, delete_local_file, delete_s3_file, load_data
+from jobs import get_jobs, pop_current_job, read_job, \
+    download_data, delete_local_file, delete_s3_file, load_data, \
+        write_data, generate_job
+    
 
 ###Logging###
 log_path = os.path.join(os.getcwd(), 'debug.log')
@@ -27,40 +27,36 @@ logging.basicConfig(filename=log_path, level=logging.INFO)
 
 ### Processing functions ###
 def process_text(text):
-    doc = nlp(text)
+    blob = TextBlob(text)
 
-    # Defining parts of speech to keep in tokens and lemmas
-    POS = ['ADJ', 'NOUN', 'PROPN', 'VERB', 'ADV', 'INTJ']
+    sentiment = blob.sentiment
+    """sentiment is a tuple
+        
+        sentiment[0] = polarity 
+        range: -1 (most negative) to 1 (most positive)
 
-    # Getting lemmas and tokens
-    lemmas = []
-    tokens = []
-    for token in doc:
-        if ((token.is_stop != True) 
-        and (token.is_punct != True) 
-        and (token.pos_ in POS)):
-            tokens.append(token.text)
-            lemmas.append(token.lemma_)
+        sentiment[1] = objectivity
+        range: 0 (most subjective) to 1 (most objective)
+    """
+    return sentiment
 
-    return (tokens, lemmas)
-
-def get_tokens(tuple):
+def get_polarity(tuple):
     return tuple[0]
 
-def get_lemmas(tuple):
+def get_objectivity(tuple):
     return tuple[1]
 
-def filter_tokens(df):
-    df['tuple'] = df.text.apply(process_text)
-    df['tokens'] = df.tuple.apply(get_tokens)
-    df['lemmas'] = df.tuple.apply(get_lemmas)
-    df = df.filter(['review_id', 'tokens', 'lemmas']) 
+def add_sentiment(df):
+    df['sentiment'] = df.text.apply(process_text)
+    df['polarity'] = df.tuple.apply(get_polarity)
+    df['objectivity'] = df.tuple.apply(get_objectivity)
+    df = df.filter(['review_id', 'polarity', 'objectivity']) 
     return df
 
 if __name__ == "__main__":
-    main_logger = logging.getLogger(__name__+" Token Fixer")
+    main_logger = logging.getLogger(__name__+" Sentiment Adder")
 
-    num_jobs = len(get_jobs('retoken')) # No module creates retoken jobs.  Manually create these.
+    num_jobs = len(get_jobs('sentiment')) # No module creates retoken jobs.  Manually create these.
 
     for i in range(num_jobs):
         # Get a job and read out the datapath
@@ -72,12 +68,14 @@ if __name__ == "__main__":
         # Load the data
         datapath = download_data(asset)
         data = load_data(datapath)
-        filtered = filter_tokens(data)
+        sentiment_df = add_sentiment(data)
 
-        # TODO still need to write to the database
-        # filtered is just a filtered database
+        # Write Data to s3
+        savepath = asset.split('/')[-1].split('.')[0] + '_sentiment'
+        write_data(data=sentiment_df, savepath=savepath)
 
-        ### import modin.pandas as pd (works the same but allows multithreading)
+        # Generate POST Job
+        generate_job(savepath, 'POST')
 
         # Cleanup
         delete_local_file(datapath)
