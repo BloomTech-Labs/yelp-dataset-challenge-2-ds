eb91d00f2d222b7f907f4eed0c044732287c5e69
Vincent Brandon <vincenta..brandon@gmail.com>
2019-11-24 19:09:47 +0000

    Added generate jobs dry_run test

--- awstools/awstools/test_jobs.py
+++ awstools/awstools/test_jobs.py
@@ -7,6 +7,7 @@ Includes tests for jobs
 import os
 import jobs
 import unittest
+import warnings
 
 ###############
 ### s3 Tests###
@@ -20,6 +21,7 @@ class TestJobs(unittest.TestCase):
 
     def setUp(self):
         # Create local job for upload
+        warnings.filterwarnings("ignore", category=ResourceWarning, message="unclosed.*<ssl.SSLSocket.*>")
         jobs.generate_job('test', 'test', dry_run=False)  # Tests generate_job to some extent
         self.jobs = jobs.get_jobs(job_type='test')
 
@@ -27,6 +29,30 @@ class TestJobs(unittest.TestCase):
         # Check that a bucket object is returned via context manager and it is available globally.
         jobs.get_bucket()
         self.assertIsNotNone(jobs.g.bucket)
+
+    def test_generate_job(self):
+        # Check that save paths and job data match inputs (dry-run test)
+        # generate jobs returns (temp_job_path, job_name, job_data)
+        objectpath = 'test_object_path'
+        job_type = 'TEST'
+        tablename = 'test_table'
+        job_name = ''.join([job_type, '_', objectpath.split('/')[-1], '_job.json'])
+        temp_job_path = '/tmp/'+job_name
+
+        job_data = {
+        'file': objectpath,
+        'tablename': tablename,
+        'test_kwarg': "test_data_value",
+            }
+
+        job_return = jobs.generate_job(
+            objectpath='test_object_path',
+            job_type='TEST',
+            tablename='test_table',
+            test_kwarg="test_data_value",
+             )
+
+        self.assertListEqual(list((temp_job_path, job_name, job_data)), list(job_return))
 
     # def test_read_job(self):
     #     # Check contents returns non-null (bucket selected must not be empty)
