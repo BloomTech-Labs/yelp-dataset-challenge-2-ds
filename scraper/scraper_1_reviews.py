e6db1047b903f6474811e1ede91cb83ef0e71db5
newnativeabq <vincent.a.brandon@gmail.com>
2020-01-05 09:25:39 -0700

    begun reviews scraper

---
+++ scraper/scraper_1_reviews.py
@@ -0,0 +1,7 @@
+"""
+Scraper 1: Reviews
+    Use ListScraper to query Yelp Fusion API for available reviews and write to database.
+"""
+
+from scrapers import ListScraper
+from read_query import non_reviewed_businesses
